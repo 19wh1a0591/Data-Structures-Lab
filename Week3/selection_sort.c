0ffc0c84339b8ca1bcc6a6fe7bd701de3cf8ba31
19wh1a0591 <69664384+19wh1a0591@users.noreply.github.com>
2020-08-19 20:38:13 +0530

    Add files via upload

---
+++ Week3/selection_sort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void selection_sort(int a[100],int n)
+{
+	int min,i,j,t;
+	for(i=0;i<n;i++)
+	{
+		min = i;
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<a[min])
+			{
+				min = j;
+			}
+			t = a[i];
+			a[i] = a[min];
+			a[min] = t;
+		}
+	}
+}
+
+int main()
+{
+	int n,i;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	selection_sort(a,n);
+	printf("Elements after sorting\n");
+	for(i=0;i<n;i++)
+		printf("%d\t",a[i]);
+}	
